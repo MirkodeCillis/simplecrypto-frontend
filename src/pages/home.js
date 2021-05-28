ca5bcf614e3bcbf9e46f0c701f66f9860a2fc8d8
Mirko de Cillis <decillis.web@gmail.com>
2021-05-28 06:54:39 +0200

    sect1 in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,27 @@ export default function Home() {
 
     return (
         <div>
-
+            <div className="columns">
+                <section className="column is-half-desktop is-full-mobile home-section sect1">
+                    <div className="home-container centered">
+                        <span className="pretitle sect1">
+                            it's just a
+                        </span>
+                        <img className="title" src="/icons/logo_white.svg" alt="Simplecrypto"/>
+                    </div>
+                </section>
+                <section className="column is-half-desktop is-full-mobile home-section sect2">
+    ciaoaoo
+                </section>
+            </div>
+            <div className="columns">
+                <section className="column is-half-desktop is-full-mobile home-section sect3">
+                    ciciccoc
+                </section>
+                <section className="column is-half-desktop is-full-mobile home-section sect4">
+                    ciciusic
+                </section>
+            </div>
         </div>
     );
 }
